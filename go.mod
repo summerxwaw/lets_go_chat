544ddd96b0630fdf68b0fe596cbb477364c82ce5
dmytro_ostrianskyi <dmytro_ostrianskyi@epam.com>
2024-02-02 01:42:54 +0200

    init commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/summerxwaw/lets_go_chat
+
+go 1.21.4
+
+require golang.org/x/crypto v0.18.0
---
+++ pkg/hasher/hasher.go
@@ -0,0 +1,17 @@
+package hasher
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+// HashPassword hashes the password using bcrypt
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes), err
+}
+
+// CheckPasswordHash compares raw password with hashed password
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
